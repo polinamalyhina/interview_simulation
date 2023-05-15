8734ba76c1165aa8360f9a397130da5acf80fbf5
Polina <polja.malygina@gmail.com>
2023-05-15 16:33:48 +0300

    Update settings.py

--- interview/settings.py
+++ interview/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['interviewsimulation.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['interviewsimulation.herokuapp.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
